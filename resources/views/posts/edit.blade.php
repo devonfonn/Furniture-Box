113b83dad30a7c5d2f9ffdb00a8300399c2cd618
devonfonn <oxo28dphoto@gmail.com>
2024-02-15 02:41:40 +0900

    投稿編集機能

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+   <head>
+       <meta charset="utf-8">
+       <title>編集</title>
+      <!-- Fonts -->
+      <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+      </head>
+      <body>
+          <h1 class="title">編集画面</h1>
+          <div class="content">
+              <form action="/posts/{{ $post->id }}" method="POST">
+                  @csrf
+                  @method('PUT')
+                  <div class='content__title'>
+                      <h2>Title</h2>
+                      <input type='text' name='post[title]' value="{{ $post->title }}">
+                  </div>
+                      <div class='content__caption'>
+                          <h2>Caption</h2>
+                          <input type='text' name='post[caption]' value="{{ $post->caption }}">
+                      </div>
+                      <input type="submit" value="保存">
+              </form>
+          </div>
+    　</body>
